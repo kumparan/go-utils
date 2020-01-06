14ac99d4fe04126bc2676ffa6cf3abe76e2555cb
Samuel Mulatua <samuel.jeremy@kumparan.com>
2020-01-06 14:04:45 +0700

    refactor: expose consts

--- media_test.go
+++ media_test.go
@@ -8,6 +8,6 @@ import (
 
 func Test_GeneratePushNotificationMediaURL(t *testing.T) {
 	externalMediaURL := "https://this.is/test/image/upload/123/234.jpg"
-	res := GeneratePushNotificationMediaURL("https://this.is", externalMediaURL, _LargeIcon)
+	res := GeneratePushNotificationMediaURL("https://this.is", externalMediaURL, LargeIcon)
 	assert.Equal(t, "https://this.is/image/upload/fl_progressive,fl_lossy,c_fill,g_face,q_auto:best,w_256,ar_1:1/123/234.jpg", res)
 }
