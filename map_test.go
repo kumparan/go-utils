68dfc05a94ff1a627de0c0f13fffe63ea652c7ad
Ahmad Nuzirwan <ahmad.nuzirwan@kumparan.com>
2020-03-06 15:49:37 +0700

    feature: add lower map key

---
+++ map_test.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_LowerMapStringKey(t *testing.T) {
+	value := map[string]interface{}{
+		"storyID" : "testing",
+	}
+	res := LowerMapStringKey(value)
+	assert.Equal(t, res["storyid"].(string), "testing")
+}
