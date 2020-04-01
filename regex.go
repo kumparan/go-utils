4342f5bd5e49e7fe6aaae8ff90a3fba0f54da74a
Riski Midi Wardana <riskimidiw@gmail.com>
2020-04-01 09:24:52 +0700

    feature: add alphanumspace regex

--- regex.go
+++ regex.go
@@ -9,3 +9,8 @@ func RegexEmail() *regexp.Regexp {
 	rgxEmail, _ := regexp.Compile("^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$")
 	return rgxEmail
 }
+
+// RegexAlphaNumSpace return regular expression for alphanumeric character with space
+func RegexAlphaNumSpace() *regexp.Regexp {
+	return regexp.MustCompile("^[a-zA-Z0-9 ]+$")
+}
--- regex_test.go
+++ regex_test.go
@@ -23,3 +23,44 @@ func Test_RegexEmail(t *testing.T) {
 		}
 	})
 }
+
+func Test_RegexAplhaNumSpace(t *testing.T) {
+	tests := []struct {
+		Name        string
+		Word        string
+		ExpectMatch bool
+	}{
+		{
+			Name:        "alphabetical character",
+			Word:        "This Is Name",
+			ExpectMatch: true,
+		},
+		{
+			Name:        "numerical character",
+			Word:        "123",
+			ExpectMatch: true,
+		},
+		{
+			Name:        "alphanum character",
+			Word:        "Joni123",
+			ExpectMatch: true,
+		},
+		{
+			Name:        "alphanumspace character",
+			Word:        "Joni 123",
+			ExpectMatch: true,
+		},
+		{
+			Name:        "except alphanumspace",
+			Word:        "whatzittooya@gmail.com",
+			ExpectMatch: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			alphaNumSpace := RegexAlphaNumSpace()
+			assert.Equal(t, test.ExpectMatch, alphaNumSpace.MatchString(test.Word))
+		})
+	}
+}
