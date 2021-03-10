b0046805455f25bcfb22260c303cdc8eb561a7a6
Agung Hariadi Tedja <agunghariaditedja@yahoo.com>
2021-03-10 10:50:04 +0700

    feature: add money formatter for multiple currencies

--- money.go
+++ money.go
@@ -3,10 +3,30 @@ package utils
 import (
 	"github.com/leekchan/accounting"
 	"github.com/shopspring/decimal"
+	"golang.org/x/text/currency"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+	"golang.org/x/text/number"
 )
 
 // FormatToIndonesianMoney format money into Indonesian
 // example: Rp10.000.000
 func FormatToIndonesianMoney(dec decimal.Decimal) string {
 	return "Rp" + accounting.FormatNumberDecimal(dec, 0, ".", ",")
+}
+
+// FormatMoney format money by currency code (ISO 4217)
+func FormatMoney(value decimal.Decimal, currencyCode string) string {
+	cur := currency.MustParseISO(currencyCode)
+	scale, _ := currency.Cash.Rounding(cur) // fractional digits
+
+	unit, _ := value.Float64()
+	dec := number.Decimal(unit, number.Scale(scale))
+
+	p := message.NewPrinter(language.English)
+	if currencyCode == "IDR" {
+		p = message.NewPrinter(language.Indonesian)
+	}
+
+	return p.Sprintf("%v%v", currency.Symbol(cur), dec)
 }
--- money_test.go
+++ money_test.go
@@ -1,15 +1,33 @@
 package utils
 
 import (
+	"testing"
+
 	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
-func Test_FormatToIndonesianMoney(t *testing.T)  {
+func Test_FormatToIndonesianMoney(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		price := decimal.NewFromFloat(10000000.88)
 		result := FormatToIndonesianMoney(price)
-		assert.Equal(t, result,"Rp10.000.001" )
+		assert.Equal(t, result, "Rp10.000.001")
 	})
+}
+
+func TestFormatMoney(t *testing.T) {
+	assert.Equal(t, "Rp10.000.000", FormatMoney(decimal.NewFromFloat(10000000), "IDR"))
+	assert.Equal(t, "Rp10.000.001", FormatMoney(decimal.NewFromFloat(10000000.88), "IDR"))
+	assert.Equal(t, "$1.49", FormatMoney(decimal.NewFromFloat(1.49), "USD"))
+	assert.Equal(t, "$1.49", FormatMoney(decimal.NewFromFloat(1.4889), "USD"))
+	assert.Equal(t, "¥149", FormatMoney(decimal.NewFromFloat(149), "JPY"))
+	assert.Equal(t, "¥149", FormatMoney(decimal.NewFromFloat(148.89), "JPY"))
+	assert.Equal(t, "€1.49", FormatMoney(decimal.NewFromFloat(1.49), "EUR"))
+	assert.Equal(t, "€1.49", FormatMoney(decimal.NewFromFloat(1.4889), "EUR"))
+	assert.Equal(t, "£1.49", FormatMoney(decimal.NewFromFloat(1.49), "GBP"))
+	assert.Equal(t, "£1.49", FormatMoney(decimal.NewFromFloat(1.4889), "GBP"))
+	assert.Equal(t, "A$1.49", FormatMoney(decimal.NewFromFloat(1.49), "AUD"))
+	assert.Equal(t, "A$1.49", FormatMoney(decimal.NewFromFloat(1.4889), "AUD"))
+	assert.Equal(t, "CA$1.49", FormatMoney(decimal.NewFromFloat(1.49), "CAD"))
+	assert.Equal(t, "CA$1.49", FormatMoney(decimal.NewFromFloat(1.4889), "CAD"))
 }
