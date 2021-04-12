cd041ee7ed0b941930c0059e7dd9044ba8c196c3
Ivan <ivan.petrus@kumparan.com>
2021-04-12 15:56:15 +0700

    refactor: using sync.Once for concurrency safe

--- striphtml.go
+++ striphtml.go
@@ -1,15 +1,22 @@
 package utils
 
-import "github.com/microcosm-cc/bluemonday"
+import (
+	"sync"
+
+	"github.com/microcosm-cc/bluemonday"
+)
 
 var (
-	_bmStrict *bluemonday.Policy
+	bmStrict *bluemonday.Policy
+	once     sync.Once
 )
 
 // StripHTML strips all HTML from a string, duh
 func StripHTML(s string) string {
-	if _bmStrict == nil {
-		_bmStrict = bluemonday.StrictPolicy()
-	}
-	return _bmStrict.Sanitize(s)
+	once.Do(func() {
+		if bmStrict == nil {
+			bmStrict = bluemonday.StrictPolicy()
+		}
+	})
+	return bmStrict.Sanitize(s)
 }
