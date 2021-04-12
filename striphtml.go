e3ef26d55f65b8ec33b6fdb3e544d91dfd0e7866
Ivan <ivan.petrus@kumparan.com>
2021-04-12 16:05:58 +0700

    feature: strip HTML (#16)
    * feature: strip HTML

---
+++ striphtml.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"sync"
+
+	"github.com/microcosm-cc/bluemonday"
+)
+
+var (
+	bmStrict *bluemonday.Policy
+	once     sync.Once
+)
+
+// StripHTML strips all HTML from a string, duh
+func StripHTML(s string) string {
+	once.Do(func() {
+		bmStrict = bluemonday.StrictPolicy()
+	})
+	return bmStrict.Sanitize(s)
+}
---
+++ striphtml_test.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_StripHTML(t *testing.T) {
+	t.Run("no html", func(t *testing.T) {
+		assert.Equal(t, "podo wae masbro",
+			StripHTML("podo wae masbro"))
+	})
+
+	t.Run("with html", func(t *testing.T) {
+		assert.Equal(t, "dang sarupa bah",
+			StripHTML("<a href=\"http://lappetkaw.com\">dang sarupa bah</a>"))
+	})
+
+	t.Run("with incomplete html", func(t *testing.T) {
+		assert.Equal(t, "&gt;plz click hier",
+			StripHTML("><a href='https://www.downloadmoreram.com'>plz click hier"))
+	})
+}
