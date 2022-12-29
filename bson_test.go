22f3f428bc7d0ee7c24c6b244813fda7fa78a6c1
Agung Hariadi Tedja <agung.tedja@kumparan.com>
2022-12-29 11:03:35 +0700

    berutc ria

--- bson_test.go
+++ bson_test.go
@@ -11,8 +11,8 @@ import (
 func TestTimeFromObjectIDHex(t *testing.T) {
 	t.Run("valid hex objectID", func(t *testing.T) {
 		cases := map[string]time.Time{
-			"63acfc824ffda9000ee65045": time.Date(2022, time.December, 29, 7, 33, 38, 0, time.UTC),
-			"5c4b288bac1b2972d0291377": time.Date(2019, time.January, 25, 15, 17, 31, 0, time.UTC),
+			"63acfc824ffda9000ee65045": time.Date(2022, time.December, 29, 7, 33, 38, 0, time.Local),
+			"5c4b288bac1b2972d0291377": time.Date(2019, time.January, 25, 15, 17, 31, 0, time.Local),
 		}
 
 		for k, v := range cases {
