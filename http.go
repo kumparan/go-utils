12cfd1cd944dba2057fab94cf859b9cdecc6ee0c
Moh Achun Armando <achunarmando@gmail.com>
2023-10-09 10:30:53 +0700

    feature: add DoHTTPRequest generic method (#57)
    * feature: add DoHTTPRequest generic method
    
    * fix
    
    * fix

---
+++ http.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/context/ctxhttp"
+)
+
+type httpResponse interface {
+	IsHTTPResponse()
+}
+
+// DoHTTPRequest generic do http request using ctxhttp
+func DoHTTPRequest[T httpResponse](ctx context.Context, httpClient *http.Client, httpRequest *http.Request) (respStatusCode int, respBody *T, err error) {
+	logger := log.WithFields(log.Fields{
+		"ctx":         DumpIncomingContext(ctx),
+		"httpRequest": Dump(httpRequest),
+	})
+
+	httpResp, err := ctxhttp.Do(ctx, httpClient, httpRequest)
+	if err != nil {
+		logger.Error(err)
+		return respStatusCode, nil, err
+	}
+
+	respInBytes, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		logger.Error(err)
+		return httpResp.StatusCode, nil, err
+	}
+	defer func() {
+		_ = httpResp.Body.Close()
+	}()
+
+	var resp T
+	err = json.Unmarshal(respInBytes, &resp)
+	if err != nil {
+		logger.WithField("respInBytes", string(respInBytes)).Error(err)
+		return httpResp.StatusCode, nil, err
+	}
+	return httpResp.StatusCode, &resp, nil
+}
