1d1a22fa20279681e75927389e42496fce58301b
Moh Achun Armando <mohamad.armando@kumparan.com>
2024-01-18 13:41:40 +0700

    feature: add IsURLReachable

--- url.go
+++ url.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"net/http"
 	"net/url"
 	"path"
 )
@@ -15,4 +16,17 @@ func JoinURL(baseURL string, pathElements ...string) (string, error) {
 	var elements []string
 	u.Path = path.Join(append(append(elements, u.Path), pathElements...)...)
 	return u.String(), nil
+}
+
+// IsURLReachable check is the url reachable
+func IsURLReachable(url string) bool {
+	res, err := http.Head(url) //nolint:gosec
+	if err != nil {
+		return false
+	}
+	defer func() {
+		_ = res.Body.Close()
+	}()
+
+	return res.StatusCode == http.StatusOK
 }
