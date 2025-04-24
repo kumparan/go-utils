ec55b929d1daae03f9e84dc7778040925bc4ed4d
fajrifernanda <fajri.fernanda@kumparan.com>
2025-04-24 11:56:49 +0700

    fix test

--- media_test.go
+++ media_test.go
@@ -9,5 +9,5 @@ import (
 func Test_GeneratePushNotificationMediaURL(t *testing.T) {
 	externalMediaURL := "https://this.is/test/image/upload/123/234.jpg"
 	res := GeneratePushNotificationMediaURL("https://this.is", externalMediaURL, LargeIcon)
-	assert.Equal(t, "https://this.is/image/upload/fl_progressive,fl_lossy,c_fill,g_face,q_auto:best,w_256,ar_1:1/123/234.jpg", res)
+	assert.Equal(t, "https://this.is/image/upload/fl_progressive,fl_lossy,c_fill,g_face,q_auto:best,w_256,ar_1:1,f_jpeg/123/234.jpg", res)
 }
